b8b532efcf386df1da8940f30869083d402dd1bd
Anton <anton.nurgaliev@mail.ru>
2022-05-03 22:10:19 +1000

    fix main content
    fix .content and remake row with video

--- src/components/Videolist/index.jsx
+++ src/components/Videolist/index.jsx
@@ -5,152 +5,272 @@ import avatar from "../../assets/Avatar.svg";
 
 function Videolist() {
   return (
-    <ul className="main__video-list content">
-      <li className="video-list__video-item">
-        <img src={preview} alt="" className="video-item__image" />
-        <div className="video-item__info-block">
-          <img id="" src={avatar} alt="" className="info-block__avatar" />
-          <div className="info-block__text">
-            <p id="" className="text__description">
-              Long video name, but it couldn’t be longer then 2 rows. Next
-              symbol is...
-            </p>
-            <p id="" className="text__author">
-              Author Name
-            </p>
+    <div className="main__video-list content">
+      <ul className="video-list__row">
+        <li className="video-list__video-item">
+          <img src={preview} alt="" className="video-item__image" />
+          <div className="video-item__info-block">
+            <img id="" src={avatar} alt="" className="info-block__avatar" />
+            <div className="info-block__text">
+              <p id="" className="text__description">
+                Long video name, but it couldn’t be longer then 2 rows. Next
+                symbol is...
+              </p>
+              <p id="" className="text__author">
+                Author Name
+              </p>
+            </div>
+            <img src="public/more.svg" alt="" className="info-block__more" />
           </div>
-          <img src="public/more.svg" alt="" className="info-block__more" />
-        </div>
-      </li>
-      <li className="video-list__video-item">
-        <img src={preview} alt="" className="video-item__image" />
-        <div className="video-item__info-block">
-          <img id="" src={avatar} alt="" className="info-block__avatar" />
-          <div className="info-block__text">
-            <p id="" className="text__description">
-              Long video name, but it couldn’t be longer then 2 rows. Next
-              symbol is...
-            </p>
-            <p id="" className="text__author">
-              Author Name
-            </p>
+        </li>
+        <li className="video-list__video-item">
+          <img src={preview} alt="" className="video-item__image" />
+          <div className="video-item__info-block">
+            <img id="" src={avatar} alt="" className="info-block__avatar" />
+            <div className="info-block__text">
+              <p id="" className="text__description">
+                Long video name, but it couldn’t be longer then 2 rows. Next
+                symbol is...
+              </p>
+              <p id="" className="text__author">
+                Author Name
+              </p>
+            </div>
+            <img src="public/more.svg" alt="" className="info-block__more" />
           </div>
-          <img src="public/more.svg" alt="" className="info-block__more" />
-        </div>
-      </li>
-      <li className="video-list__video-item">
-        <img src={preview} alt="" className="video-item__image" />
-        <div className="video-item__info-block">
-          <img id="" src={avatar} alt="" className="info-block__avatar" />
-          <div className="info-block__text">
-            <p id="" className="text__description">
-              Long video name, but it couldn’t be longer then 2 rows. Next
-              symbol is...
-            </p>
-            <p id="" className="text__author">
-              Author Name
-            </p>
+        </li>
+        <li className="video-list__video-item">
+          <img src={preview} alt="" className="video-item__image" />
+          <div className="video-item__info-block">
+            <img id="" src={avatar} alt="" className="info-block__avatar" />
+            <div className="info-block__text">
+              <p id="" className="text__description">
+                Long video name, but it couldn’t be longer then 2 rows. Next
+                symbol is...
+              </p>
+              <p id="" className="text__author">
+                Author Name
+              </p>
+            </div>
+            <img src="public/more.svg" alt="" className="info-block__more" />
           </div>
-          <img src="public/more.svg" alt="" className="info-block__more" />
-        </div>
-      </li>{" "}
-      <li className="video-list__video-item">
-        <img src={preview} alt="" className="video-item__image" />
-        <div className="video-item__info-block">
-          <img id="" src={avatar} alt="" className="info-block__avatar" />
-          <div className="info-block__text">
-            <p id="" className="text__description">
-              Long video name, but it couldn’t be longer then 2 rows. Next
-              symbol is...
-            </p>
-            <p id="" className="text__author">
-              Author Name
-            </p>
+        </li>
+        <li className="video-list__video-item">
+          <img src={preview} alt="" className="video-item__image" />
+          <div className="video-item__info-block">
+            <img id="" src={avatar} alt="" className="info-block__avatar" />
+            <div className="info-block__text">
+              <p id="" className="text__description">
+                Long video name, but it couldn’t be longer then 2 rows. Next
+                symbol is...
+              </p>
+              <p id="" className="text__author">
+                Author Name
+              </p>
+            </div>
+            <img src="public/more.svg" alt="" className="info-block__more" />
           </div>
-          <img src="public/more.svg" alt="" className="info-block__more" />
-        </div>
-      </li>{" "}
-      <li className="video-list__video-item">
-        <img src={preview} alt="" className="video-item__image" />
-        <div className="video-item__info-block">
-          <img id="" src={avatar} alt="" className="info-block__avatar" />
-          <div className="info-block__text">
-            <p id="" className="text__description">
-              Long video name, but it couldn’t be longer then 2 rows. Next
-              symbol is...
-            </p>
-            <p id="" className="text__author">
-              Author Name
-            </p>
+        </li>
+      </ul>
+      <ul className="video-list__row">
+        <li className="video-list__video-item">
+          <img src={preview} alt="" className="video-item__image" />
+          <div className="video-item__info-block">
+            <img id="" src={avatar} alt="" className="info-block__avatar" />
+            <div className="info-block__text">
+              <p id="" className="text__description">
+                Long video name, but it couldn’t be longer then 2 rows. Next
+                symbol is...
+              </p>
+              <p id="" className="text__author">
+                Author Name
+              </p>
+            </div>
+            <img src="public/more.svg" alt="" className="info-block__more" />
           </div>
-          <img src="public/more.svg" alt="" className="info-block__more" />
-        </div>
-      </li>{" "}
-      <li className="video-list__video-item">
-        <img src={preview} alt="" className="video-item__image" />
-        <div className="video-item__info-block">
-          <img id="" src={avatar} alt="" className="info-block__avatar" />
-          <div className="info-block__text">
-            <p id="" className="text__description">
-              Long video name, but it couldn’t be longer then 2 rows. Next
-              symbol is...
-            </p>
-            <p id="" className="text__author">
-              Author Name
-            </p>
+        </li>
+        <li className="video-list__video-item">
+          <img src={preview} alt="" className="video-item__image" />
+          <div className="video-item__info-block">
+            <img id="" src={avatar} alt="" className="info-block__avatar" />
+            <div className="info-block__text">
+              <p id="" className="text__description">
+                Long video name, but it couldn’t be longer then 2 rows. Next
+                symbol is...
+              </p>
+              <p id="" className="text__author">
+                Author Name
+              </p>
+            </div>
+            <img src="public/more.svg" alt="" className="info-block__more" />
           </div>
-          <img src="public/more.svg" alt="" className="info-block__more" />
-        </div>
-      </li>{" "}
-      <li className="video-list__video-item">
-        <img src={preview} alt="" className="video-item__image" />
-        <div className="video-item__info-block">
-          <img id="" src={avatar} alt="" className="info-block__avatar" />
-          <div className="info-block__text">
-            <p id="" className="text__description">
-              Long video name, but it couldn’t be longer then 2 rows. Next
-              symbol is...
-            </p>
-            <p id="" className="text__author">
-              Author Name
-            </p>
+        </li>
+        <li className="video-list__video-item">
+          <img src={preview} alt="" className="video-item__image" />
+          <div className="video-item__info-block">
+            <img id="" src={avatar} alt="" className="info-block__avatar" />
+            <div className="info-block__text">
+              <p id="" className="text__description">
+                Long video name, but it couldn’t be longer then 2 rows. Next
+                symbol is...
+              </p>
+              <p id="" className="text__author">
+                Author Name
+              </p>
+            </div>
+            <img src="public/more.svg" alt="" className="info-block__more" />
           </div>
-          <img src="public/more.svg" alt="" className="info-block__more" />
-        </div>
-      </li>{" "}
-      <li className="video-list__video-item">
-        <img src={preview} alt="" className="video-item__image" />
-        <div className="video-item__info-block">
-          <img id="" src={avatar} alt="" className="info-block__avatar" />
-          <div className="info-block__text">
-            <p id="" className="text__description">
-              Long video name, but it couldn’t be longer then 2 rows. Next
-              symbol is...
-            </p>
-            <p id="" className="text__author">
-              Author Name
-            </p>
+        </li>
+        <li className="video-list__video-item">
+          <img src={preview} alt="" className="video-item__image" />
+          <div className="video-item__info-block">
+            <img id="" src={avatar} alt="" className="info-block__avatar" />
+            <div className="info-block__text">
+              <p id="" className="text__description">
+                Long video name, but it couldn’t be longer then 2 rows. Next
+                symbol is...
+              </p>
+              <p id="" className="text__author">
+                Author Name
+              </p>
+            </div>
+            <img src="public/more.svg" alt="" className="info-block__more" />
           </div>
-          <img src="public/more.svg" alt="" className="info-block__more" />
-        </div>
-      </li>{" "}
-      <li className="video-list__video-item">
-        <img src={preview} alt="" className="video-item__image" />
-        <div className="video-item__info-block">
-          <img id="" src={avatar} alt="" className="info-block__avatar" />
-          <div className="info-block__text">
-            <p id="" className="text__description">
-              Long video name, but it couldn’t be longer then 2 rows. Next
-              symbol is...
-            </p>
-            <p id="" className="text__author">
-              Author Name
-            </p>
+        </li>
+      </ul>
+      <ul className="video-list__row">
+        <li className="video-list__video-item">
+          <img src={preview} alt="" className="video-item__image" />
+          <div className="video-item__info-block">
+            <img id="" src={avatar} alt="" className="info-block__avatar" />
+            <div className="info-block__text">
+              <p id="" className="text__description">
+                Long video name, but it couldn’t be longer then 2 rows. Next
+                symbol is...
+              </p>
+              <p id="" className="text__author">
+                Author Name
+              </p>
+            </div>
+            <img src="public/more.svg" alt="" className="info-block__more" />
           </div>
-          <img src="public/more.svg" alt="" className="info-block__more" />
-        </div>
-      </li>
-    </ul>
+        </li>
+        <li className="video-list__video-item">
+          <img src={preview} alt="" className="video-item__image" />
+          <div className="video-item__info-block">
+            <img id="" src={avatar} alt="" className="info-block__avatar" />
+            <div className="info-block__text">
+              <p id="" className="text__description">
+                Long video name, but it couldn’t be longer then 2 rows. Next
+                symbol is...
+              </p>
+              <p id="" className="text__author">
+                Author Name
+              </p>
+            </div>
+            <img src="public/more.svg" alt="" className="info-block__more" />
+          </div>
+        </li>
+        <li className="video-list__video-item">
+          <img src={preview} alt="" className="video-item__image" />
+          <div className="video-item__info-block">
+            <img id="" src={avatar} alt="" className="info-block__avatar" />
+            <div className="info-block__text">
+              <p id="" className="text__description">
+                Long video name, but it couldn’t be longer then 2 rows. Next
+                symbol is...
+              </p>
+              <p id="" className="text__author">
+                Author Name
+              </p>
+            </div>
+            <img src="public/more.svg" alt="" className="info-block__more" />
+          </div>
+        </li>
+        <li className="video-list__video-item">
+          <img src={preview} alt="" className="video-item__image" />
+          <div className="video-item__info-block">
+            <img id="" src={avatar} alt="" className="info-block__avatar" />
+            <div className="info-block__text">
+              <p id="" className="text__description">
+                Long video name, but it couldn’t be longer then 2 rows. Next
+                symbol is...
+              </p>
+              <p id="" className="text__author">
+                Author Name
+              </p>
+            </div>
+            <img src="public/more.svg" alt="" className="info-block__more" />
+          </div>
+        </li>
+      </ul>
+      <ul className="video-list__row">
+        <li className="video-list__video-item">
+          <img src={preview} alt="" className="video-item__image" />
+          <div className="video-item__info-block">
+            <img id="" src={avatar} alt="" className="info-block__avatar" />
+            <div className="info-block__text">
+              <p id="" className="text__description">
+                Long video name, but it couldn’t be longer then 2 rows. Next
+                symbol is...
+              </p>
+              <p id="" className="text__author">
+                Author Name
+              </p>
+            </div>
+            <img src="public/more.svg" alt="" className="info-block__more" />
+          </div>
+        </li>
+        <li className="video-list__video-item">
+          <img src={preview} alt="" className="video-item__image" />
+          <div className="video-item__info-block">
+            <img id="" src={avatar} alt="" className="info-block__avatar" />
+            <div className="info-block__text">
+              <p id="" className="text__description">
+                Long video name, but it couldn’t be longer then 2 rows. Next
+                symbol is...
+              </p>
+              <p id="" className="text__author">
+                Author Name
+              </p>
+            </div>
+            <img src="public/more.svg" alt="" className="info-block__more" />
+          </div>
+        </li>
+        <li className="video-list__video-item">
+          <img src={preview} alt="" className="video-item__image" />
+          <div className="video-item__info-block">
+            <img id="" src={avatar} alt="" className="info-block__avatar" />
+            <div className="info-block__text">
+              <p id="" className="text__description">
+                Long video name, but it couldn’t be longer then 2 rows. Next
+                symbol is...
+              </p>
+              <p id="" className="text__author">
+                Author Name
+              </p>
+            </div>
+            <img src="public/more.svg" alt="" className="info-block__more" />
+          </div>
+        </li>
+        <li className="video-list__video-item">
+          <img src={preview} alt="" className="video-item__image" />
+          <div className="video-item__info-block">
+            <img id="" src={avatar} alt="" className="info-block__avatar" />
+            <div className="info-block__text">
+              <p id="" className="text__description">
+                Long video name, but it couldn’t be longer then 2 rows. Next
+                symbol is...
+              </p>
+              <p id="" className="text__author">
+                Author Name
+              </p>
+            </div>
+            <img src="public/more.svg" alt="" className="info-block__more" />
+          </div>
+        </li>
+      </ul>
+    </div>
   );
 }
 
